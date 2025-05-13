3439dcc4d35730e6e95128179e253b98e9ba93d0
chenbabay <chenbabay2004@gmail.com>
2025-05-13 13:18:06 +0300

    adding material class

---
+++ src/primitives/Material.java
@@ -0,0 +1,40 @@
+package primitives;
+
+/**
+ * Class representing the material properties of a surface.
+ *
+ * @author Chen Babay & Linoy Shamshian
+ */
+public class Material {
+    /**
+     * Diffuse reflection coefficient
+     */
+    public Double3 kA = Double3.ONE;
+
+    /**
+     * Default constructor
+     */
+    public Material() {}
+
+    /**
+     * Setter for kD using a Double3 parameter
+     *
+     * @param kA the diffuse reflection coefficient
+     * @return the Material object itself (for chaining)
+     */
+    public Material setKD(Double3 kA) {
+        this.kA = kA;
+        return this;
+    }
+
+    /**
+     * Setter for kA using a double parameter (converted to Double3)
+     *
+     * @param kA the diffuse reflection coefficient
+     * @return the Material object itself (for chaining)
+     */
+    public Material setKD(double kA) {
+        this.kA = new Double3(kA);
+        return this;
+    }
+}
