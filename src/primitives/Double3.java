4afbf9fb9822ca27d24d7f117a990583481bc07f
linoyshamshian <linoyshamshian@gmail.com>
2025-05-27 01:09:19 +0300

    add greaterThan

--- src/primitives/Double3.java
+++ src/primitives/Double3.java
@@ -131,4 +131,15 @@ public record Double3(double d1, double d2, double d3) {
     public boolean lowerThan(Double3 other) {
         return d1 < other.d1 && d2 < other.d2 && d3 < other.d3;
     }
+
+    /**
+     * Checks whether all the numbers are greater than a test number
+     *
+     * @param k the test number
+     * @return true if all the numbers are bigger than k, false otherwise
+     */
+
+    public boolean greaterThan(double k) {
+        return d1 > k && d2 > k && d3 > k;
+    }
 }
